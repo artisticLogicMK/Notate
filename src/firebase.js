723bd6a68a0cc03ea416ec8ffae71b5252a6f184
ArtisticLogic <mk.artisticlogic@gmail.com>
2022-10-11 17:47:06 +0100

    init

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,7 @@
 import { initializeApp } from 'firebase/app'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAz8ZfEJT1ZdyRy7M5LLkSGTLNOWsuZqx4",
-    authDomain: "upnote-app.firebaseapp.com",
-    projectId: "upnote-app",
-    storageBucket: "upnote-app.appspot.com",
-    messagingSenderId: "307795543999",
-    appId: "1:307795543999:web:9f2b45ab427fd3bc45830a"
+    //firebase configuration goes here
 }
 
 const initFirebase = initializeApp(firebaseConfig)
